b9a6708553c53f3e5cd0b236b49bbabe080bc1e0
Mary Crivelli <mary.crivelli@ibm.com>
2020-01-21 16:02:53 -0500

    [TESTS] PASS: Canary, String Check, Integer Test / FAIL: None

--- client/src/marys-tests/word-wrap.spec.js
+++ client/src/marys-tests/word-wrap.spec.js
@@ -1,6 +1,7 @@
-const wordWrap = (userInput) => {
-    if (typeof userInput != 'string') throw new Error('not a string');
-    return userInput;
+const wordWrap = (stringInput, integerInput) => {
+    if (typeof stringInput != 'string') throw new Error('not a string');
+    if (typeof integerInput != 'integer') throw new Error('not an integer');
+    return stringInput, integerInput;
 }
 
 describe('word-wrap', () => {
@@ -8,10 +9,18 @@ describe('word-wrap', () => {
         expect(true).toEqual(true);
     });
 
-    test('throw error if user input is not a string', () => {
+    test('throw error if first input is not a string', () => {
         expect(() => {
             wordWrap(0);
         }).toThrow(new Error('not a string'))
     });
+
+    test('throw error if second input is not an integer', () => {
+        expect(() => {
+            wordWrap('test');
+        }).toThrow(new Error('not an integer'))
+    });
+
+
 
 });
