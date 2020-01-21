550b843d5bb763fc7255c6acfa03e5d4b459ceef
Mary Crivelli <mary.crivelli@ibm.com>
2020-01-21 14:50:32 -0500

    Stole Esther's code, all tests passing

--- client/src/marys-tests/fahrenheit-to-celsius.spec.js
+++ client/src/marys-tests/fahrenheit-to-celsius.spec.js
@@ -1,25 +1,28 @@
-const fahrenheitToCelcius = (degreesFahrenheit) => {
-    if (typeof degreesFahrenheit !== 'number') {
-        throw new Error('the value is not a number')
-    }  return (degreesFahrenheit - 32) * 5 / 9;
-}
+import { fail } from "assert";
 
+const fahrenheitToCelsius = (degreesFahrenheit) => {
+    if(typeof degreesFahrenheit !== 'number') throw new Error('not a number');
+    return (degreesFahrenheit - 32) * (5/9);
+}
 describe('farenheit-to-celsius', () => {
     test('canary verified test tools', () => {
         expect(true).toEqual(true);
     });
-
-    test('32 degrees farenheit is 0 degrees celsius', () => {
-        expect(fahrenheitToCelcius(32)).toEqual(0);
+    test('32 degrees fahrenheit is 0 degrees celsius', () => {
+        expect(fahrenheitToCelsius(32)).toEqual(0);
     });
-
-    test('212 degrees farenheit is 100 degrees celsius', () => {
-        expect(fahrenheitToCelcius(212)).toEqual(100);
+    test('212 degrees fahrenheit is 100 degrees celsius', () => {
+        expect(fahrenheitToCelsius(212)).toEqual(100);
     });
-
     test('when value is not a number then throw an error', () => {
         expect(() => {
-            fahrenheitToCelcius('test');
-        }).toThrow(new Error('the value is not a number'));
+            fahrenheitToCelsius('test');
+        }).toThrow(new Error('not a number'));
+        try {
+            fahrenheitToCelsius('test');
+            fail('should throw exvception');
+        } catch (err) {
+            expect(err).toEqual(new Error('not a number'))
+        };
     });
 });
