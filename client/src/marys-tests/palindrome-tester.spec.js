bb5b3d37c02b4f21c326d77d1451120980b6e371
Mary Crivelli <mary.crivelli@ibm.com>
2020-01-21 15:22:27 -0500

    [TESTS] Passing: Canary, User Input is String, User Input is not Empty String / Failing: None

--- client/src/marys-tests/palindrome-tester.spec.js
+++ client/src/marys-tests/palindrome-tester.spec.js
@@ -1,5 +1,7 @@
 const palindromeChecker = (userInput) => {
     if (typeof userInput != 'string') throw new Error('not a string');
+    if (userInput.length <= 0) throw new Error('no empty strings allowed');
+
 }
 
 describe('palindrome-tester', () => {
@@ -11,6 +13,12 @@ describe('palindrome-tester', () => {
         expect(() => {
             palindromeChecker(0);
         }).toThrow(new Error('not a string'))
+    });
+
+    test('user input is not empty string', () => {
+        expect(() => {
+            palindromeChecker('');
+        }).toThrow(new Error('no empty strings allowed'))
     });
 
 });
