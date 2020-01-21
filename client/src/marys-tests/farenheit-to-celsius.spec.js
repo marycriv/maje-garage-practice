b9b3e2439d9d5c121a3aef83de5cb9b3faf4d47f
Mary Crivelli <mary.crivelli@ibm.com>
2020-01-21 14:33:55 -0500

    Tests: 32 to 0 passing / 212 to 100 failing

--- client/src/marys-tests/farenheit-to-celsius.spec.js
+++ client/src/marys-tests/farenheit-to-celsius.spec.js
@@ -1,3 +1,7 @@
+const farenheitToCelcius = () => {
+    return 0;
+}
+
 describe('farenheit-to-celsius', () => {
     test('canary verified test tools', () => {
         expect(true).toEqual(true);
@@ -5,5 +9,9 @@ describe('farenheit-to-celsius', () => {
 
     test('32 degrees farenheit is 0 degrees celsius', () => {
         expect(farenheitToCelcius(32)).toEqual(0);
+    });
+
+    test('212 degrees farenheit is 100 degrees celsius', () => {
+        expect(farenheitToCelcius(212).toEqual(100));
     });
 });
