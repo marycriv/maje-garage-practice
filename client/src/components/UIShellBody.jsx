c4146e6a58ea35492fd1101b53e1951c92db58b5
Esther Baek <esther.baek@ibm.com>
2020-01-22 14:08:59 -0500

    item submit button

--- client/src/components/UIShellBody.jsx
+++ client/src/components/UIShellBody.jsx
@@ -5,12 +5,29 @@ import BasicPage from "../pattern-components/BasicPage";
 import "../pattern-components/patterns.scss";
 
 class UIShellBody extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      itemName: ""
+    }  
+  }
+
   components = {
     "Shopping Lists Menu": ShoppingListsMenu,
     "Shopping List": ShoppingList,
     "Basic Page": BasicPage
   };
   defaultComponent = "Basic Page";
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const item = event.target[0].value;
+    this.setState({itemName: item});
+  }
 
   render() {
     let curScreen = this.defaultComponent;
@@ -19,8 +36,10 @@ class UIShellBody extends Component {
       <div className="pattern-container">
         <PatternName showDescription={true} />
         <ShoppingList />
-        <input type="text" placeholder="Item..."></input>
-        <input type="button" value="Add Item"></input>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input type="text" placeholder="Item..."></input>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
